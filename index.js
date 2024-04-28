e8c4352870e4e81ce90286df2fce11ea796d7d54
jfftck <jfftck@users.noreply.github.com>
2024-04-28 02:35:19 +0000

    Fixed JavaScript, improved styling, and added some content.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 'use strict'
-
 //import { GalleryDetails, PhotoDetails, gallery } from './gallery';
+
+const aniEnd = (e) => {
+    if (e.animationName === 'curtian') {
+        document.body.classList.remove('curtian');
+        document.body.removeEventListener('animationend', aniEnd);
+    }
+};
+
+document.body.addEventListener("click", () => {
+    if (document.body.classList.contains("blur")) {
+        document.body.focus();
+        document.body.classList.remove("blur");
+    }
+});
 
 document.body.onload = () => {
     requestAnimationFrame(() => {
-        document.body.classList.add('loaded')
-        setTimeout(() => {
-            document.body.classList.remove('curtian')
-        }, 2000)
+        document.body.classList.add('loaded');
+        document.body.addEventListener("animationend", aniEnd);
     })
-
-    document.body.addEventListener("click", () => {
-        if (document.body.classList.contains("blur")) {
-            document.body.focus()
-            document.body.classList.remove("blur")
-        }
-    })
-}
+};
